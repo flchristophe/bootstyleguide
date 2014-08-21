7a13b54d2f4bb6389294c08865aa558b34895387
Florent Christophe <f.christophe@audacy.fr>
2014-08-21 17:02:11 +0200

    Startkit v0.4
    adding markup for patterns and page-headers

--- assets/js/offcanvas.js
+++ assets/js/offcanvas.js
@@ -3,3 +3,18 @@ $(document).ready(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
+$(document).ready(function () {
+  $('[data-toggle=offcanvas-left]').click(function () {
+    $('.header-offcanvas-left').toggleClass('active')
+  });
+});
+$(document).ready(function () {
+  $('[data-toggle=offcanvas-center]').click(function () {
+    $('.header-offcanvas-center').toggleClass('active')
+  });
+});
+$(document).ready(function () {
+  $('[data-toggle=offcanvas-right]').click(function () {
+    $('.header-offcanvas-right').toggleClass('active')
+  });
+});
