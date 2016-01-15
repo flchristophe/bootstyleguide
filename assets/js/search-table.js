5e0d0e025707e17a5db249245d66177a65081bf1
Florent Christophe <f.christophe@audacy.fr>
2016-01-15 15:54:48 +0100

    Updating bootstrap & adding persona template
    Updating bootstrap & adding persona template

---
+++ assets/js/search-table.js
@@ -0,0 +1,38 @@
+// Searchable Tables
+
+$(document).ready(function () {
+
+    (function ($) {
+
+        $('#filter').keyup(function () {
+
+            var rex = new RegExp($(this).val(), 'i');
+            $('.searchable tr').hide();
+            $('.searchable tr').filter(function () {
+                return rex.test($(this).text());
+            }).show();
+
+        })
+
+    }(jQuery));
+
+});
+
+$(document)
+	.on('change', '.btn-file :file', function() {
+		var input = $(this),
+		numFiles = input.get(0).files ? input.get(0).files.length : 1,
+		label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		input.trigger('fileselect', [numFiles, label]);
+});
+$(document).ready( function() {
+	$('.btn-file :file').on('fileselect', function(event, numFiles, label) {	
+		var input = $(this).parents('.input-group').find(':text'),
+		log = numFiles > 1 ? numFiles + ' files selected' : label;
+		if( input.length ) {
+			input.val(log);
+		} else {
+			if( log ) alert(log);
+		}	
+	});
+});
